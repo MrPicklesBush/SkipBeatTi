57fccd3061e3ac6c8a1da6ffc3d7ab36e029f3fd
kevincui1034 <kevincui1034@gmail.com>
2024-12-05 00:46:18 -0800

    shadcn js fix

--- frontend/src/components/MainLayout.jsx
+++ frontend/src/components/MainLayout.jsx
@@ -1,23 +1,27 @@
-// import LeftSidebar from "./components/LeftSidebar";
-// import { ResizablePanel, ResizablePanelGroup } from "react-router-dom";
-// import { Outlet } from "react-router-dom";
+import LeftSidebar from "./components/LeftSidebar";
+import { ResizablePanel, ResizablePanelGroup } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-// const MainLayout = () => {
-//     const isMobile = false;
-//     return <div className = "h-screen bg-black text-white flex flex-col">MainLAyout</div>;
+const MainLayout = () => {
+    const isMobile = false;
 
-//         <ReizablePanelGroup>
-//             { /* Left sidebar */}
-//             <ResizablePanel defaultSize = {20} minSize = {isMobile ?0: 10} maxSize = {30}>
-//                 left sidebar
-//             </ResizablePanel>
-//             {/* Main Content*/}
+    return (
+        <div className="h-screen bg-black text-white flex flex-col">
+            MainLayout
 
-//             </ResizablePanel>
-//                 <Outlet/>
-//             </ResizablePanel>
+            <ResizablePanelGroup>
+                {/* Left sidebar */}
+                <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
+                    left sidebar
+                </ResizablePanel>
 
-//         </ReizablePanelGroup>
-// };
+                {/* Main Content */}
+                <ResizablePanel>
+                    <Outlet />
+                </ResizablePanel>
+            </ResizablePanelGroup>
+        </div>
+    );
+};
 
-// export default MainLayout;
+export default MainLayout;
