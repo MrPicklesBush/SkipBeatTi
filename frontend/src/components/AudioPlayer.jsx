dff031c92637389fb02a3cc51ad0c438b3108000
kevincui1034 <kevincui1034@gmail.com>
2024-12-05 16:15:40 -0800

    added playback functionality

--- frontend/src/components/AudioPlayer.jsx
+++ frontend/src/components/AudioPlayer.jsx
@@ -1,4 +1,43 @@
+import { usePlayerStore } from "@/stores/usePlayerStore";
+
 const AudioPlayer = () => {
-    return <div>AudioPlayer</div>;
+    const audioref = useRef<HTMLAUdioElement>(null)
+    const prevSongRef = useRef<string | null>(null);
+        const {currentSong, isPlaying, playNext} = usePlayerStore()
+
+    useEffect(() => {
+        if(isPlaying) audioRef.current?.play();
+        else audioRef.current?.pause();
+
+    }, [isPlaying]);
+
+    // handle song ends
+    useEffect(() => {
+        const audio = audioRef.current;
+        const handleEnded = () => {
+            playNext()
+        }
+        audio.addEventListener("ended", handleEnded)
+        return () => audio.removeEventListener("ended", handleEnded);
+    }, [playNext]);
+
+    // handle song changes
+    useEffect(() => {
+        if(!audioRef.current || !currentSong) return;
+
+        const audio = audioRef.current;
+        const isSongChange = prevSongRef.current !== currentSong?.audioUrl;
+        if (isSongChange) {
+            audio.src = currentSong?.audioUrl;
+
+            // resets playback position
+            audio.currentTime = 0;
+            prevSongRef.current = currentSong?.audioUrl;
+
+            if(isPlaying) audio.play();
+        }
+    }, [currentSong, isPlaying]);
+
+    return <audio ref = {audioRef} />;
 };
 export default AudioPlayer;
